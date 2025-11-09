ff846fa4ed7497e6ca10320bf88ce063e03a5c41
Nessrine Sayari <sayarinessrine589@gmail.com>
2025-11-09 19:32:29 +0100

    Ajout du code Prosit 7 (gestion des exceptions ZooFullException et InvalidAgeException)

--- src/tn/esprit/gestionzoo/entities/Main.java
+++ src/tn/esprit/gestionzoo/entities/Main.java
@@ -2,29 +2,20 @@ package tn.esprit.gestionzoo.entities;
 
 public class Main {
     public static void main(String[] args) {
-        Zoo zoo = new Zoo();
+        Zoo zoo = new Zoo("MonZoo", "Tunisie", 3);
 
-        // Ajouter des animaux aquatiques
-        Penguin p1 = new Penguin("Pingo", 3, "Antarctique", 15.5f);
-        Penguin p2 = new Penguin("Lolo", 5, "Antarctique", 12.0f);
-        Dolphin d1 = new Dolphin("Flipper", 8, "Océan", 25.0f);
-
-        zoo.addAquaticAnimal(p1);
-        zoo.addAquaticAnimal(p2);
-        zoo.addAquaticAnimal(d1);
-
-        // Afficher les méthodes swim()
-        zoo.showAllSwim();
-
-        // Tester la profondeur max
-        System.out.println("Profondeur maximale des pingouins : " + zoo.maxPenguinSwimmingDepth() + " m");
-
-        // Afficher le nombre par type
-        zoo.displayNumberOfAquaticsByType();
-
-        // Test equals()
-        Penguin p3 = new Penguin("Pingo", 3, "Antarctique", 10f);
-        System.out.println("p1 == p3 ? " + p1.equals(p3));
+        try {
+            Animal a1 = new Animal("Lion", "Afrique", 5, true);
+            Animal a2 = new Animal("Tigre", "Asie", -2, true); // âge négatif → exception
+            zoo.addAnimal(a1);
+            zoo.addAnimal(a2);
+            System.out.println("Nombre d’animaux : " + zoo.getNbAnimals());
+        }
+        catch (InvalidAgeException e) {
+            System.err.println("Erreur d’âge : " + e.getMessage());
+        }
+        catch (ZooFullException e) {
+            System.err.println("Erreur de zoo : " + e.getMessage());
+        }
     }
 }
-
--- src/tn/esprit/gestionzoo/entities/Zoo.java
+++ src/tn/esprit/gestionzoo/entities/Zoo.java
@@ -1,53 +1,28 @@
 package tn.esprit.gestionzoo.entities;
 
 public class Zoo {
-    private Aquatic[] aquaticAnimals = new Aquatic[10];
-    private int nbAquatic = 0;
+    private String name;
+    private String city;
+    private int nbrCages;
+    private Animal[] animals;
+    private int nbAnimals = 0;
 
-    // Instruction 26
-    public void addAquaticAnimal(Aquatic aquatic) {
-        if (nbAquatic < aquaticAnimals.length) {
-            aquaticAnimals[nbAquatic++] = aquatic;
-        } else {
-            System.out.println("Le zoo est plein d’animaux aquatiques !");
-        }
+    public Zoo(String name, String city, int nbrCages) {
+        this.name = name;
+        this.city = city;
+        this.nbrCages = nbrCages;
+        this.animals = new Animal[nbrCages];
     }
 
-    // Instruction 27
-    public void showAllSwim() {
-        for (int i = 0; i < nbAquatic; i++) {
-            aquaticAnimals[i].swim();
+    // ✅ Méthode demandée par l’instruction 32
+    public void addAnimal(Animal animal) throws ZooFullException {
+        if (nbAnimals >= nbrCages) {
+            throw new ZooFullException("Le zoo est plein ! Impossible d’ajouter un nouvel animal.");
         }
+        animals[nbAnimals++] = animal;
     }
 
-    // Instruction 29
-    public float maxPenguinSwimmingDepth() {
-        float maxDepth = 0;
-        for (int i = 0; i < nbAquatic; i++) {
-            if (aquaticAnimals[i] instanceof Penguin) {
-                float depth = ((Penguin) aquaticAnimals[i]).getSwimmingDepth();
-                if (depth > maxDepth) {
-                    maxDepth = depth;
-                }
-            }
-        }
-        return maxDepth;
-    }
-
-    // Instruction 30
-    public void displayNumberOfAquaticsByType() {
-        int countPenguins = 0;
-        int countDolphins = 0;
-
-        for (int i = 0; i < nbAquatic; i++) {
-            if (aquaticAnimals[i] instanceof Penguin) {
-                countPenguins++;
-            } else if (aquaticAnimals[i] instanceof Dolphin) {
-                countDolphins++;
-            }
-        }
-
-        System.out.println("Nombre de Pingouins : " + countPenguins);
-        System.out.println("Nombre de Dauphins : " + countDolphins);
+    public int getNbAnimals() {
+        return nbAnimals;
     }
 }
